6d579c7b79b476c1f80adfb48d57939535035f0a
Pedro Orellana <pedrorellana21@gmail.com>
2024-03-19 13:12:45 -0700

    start work with adding bluetooth functionality

--- app/src/main/java/com/pedroapps/smartring20/MainActivity.kt
+++ app/src/main/java/com/pedroapps/smartring20/MainActivity.kt
@@ -1,23 +1,26 @@
 package com.pedroapps.smartring20
 
+import android.Manifest
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.mbientlab.metawear.MetaWearBoard
 import com.pedroapps.smartring20.components.AppBottomBar
 import com.pedroapps.smartring20.screens.Destinations
 import com.pedroapps.smartring20.screens.DevicesScreen
@@ -26,11 +29,38 @@ import com.pedroapps.smartring20.screens.HomeScreen
 import com.pedroapps.smartring20.screens.NewDeviceScreen
 import com.pedroapps.smartring20.screens.SettingsScreen
 import com.pedroapps.smartring20.ui.theme.SmartRing20Theme
+import com.pedroapps.smartring20.viewmodels.MainViewModel
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val bluetoothPermissionsLauncher =
+              rememberLauncherForActivityResult(
+                  contract = ActivityResultContracts.RequestPermission()) { isGranted ->
+                  val message = if(isGranted) "able to perform bluetooth scans!"
+                  else "Please grant this permission for this app to work properly"
+
+                  Toast.makeText(this,message, Toast.LENGTH_SHORT).show()
+              }
+
+
+            val coarseLocationPermissionLauncher =
+                rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.RequestPermission()) { isGranted ->
+                    val message = if(isGranted) "Location permission granted!"
+                        else "please grant location permission"
+                }
+
+
+
+            LaunchedEffect(key1 = bluetoothPermissionsLauncher, key2 = coarseLocationPermissionLauncher) {
+                bluetoothPermissionsLauncher.launch(Manifest.permission.BLUETOOTH_SCAN)
+                //it doesn't say in the official documentation, but to be able to
+                //scan bluetooth devices, fine location is required
+                coarseLocationPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            }
            AppContent()
         }
     }
@@ -52,7 +82,10 @@ fun AppContent() {
 
 
 @Composable
-fun ContainerContent() {
+fun ContainerContent(
+    viewModel: MainViewModel = viewModel()
+) {
+
     val navController = rememberNavController()
     Scaffold(
         bottomBar = { AppBottomBar(navController = navController) },
@@ -68,7 +101,10 @@ fun ContainerContent() {
                     GesturesScreen(paddingValues = paddingValues)
                 }
                 composable(route = Destinations.HomeScreen) {
-                    HomeScreen(paddingValues = paddingValues)
+                    HomeScreen(
+                        paddingValues = paddingValues,
+                        startScanning = viewModel::startScanning
+                        )
                 }
                 composable(route = Destinations.NewDeviceScreen) {
                     NewDeviceScreen(paddingValues = paddingValues)
